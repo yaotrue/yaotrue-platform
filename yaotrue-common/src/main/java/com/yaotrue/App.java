967289a26af92640ec4c80e28a308c82b5ae6dd5
yaotrue <yaotrue@163.com>
2016-05-16 19:17:55 +0800

    init project

---
+++ yaotrue-common/src/main/java/com/yaotrue/App.java
@@ -0,0 +1,13 @@
+package com.yaotrue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
