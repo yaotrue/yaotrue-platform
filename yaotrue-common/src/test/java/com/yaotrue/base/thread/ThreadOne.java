b9bb75231cd4c727de6372ae20144c691184eb4d
yaotrue <yaotrue@163.com>
2016-05-26 20:10:37 +0800

    add thread test

---
+++ yaotrue-common/src/test/java/com/yaotrue/base/thread/ThreadOne.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2012 Baozun All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Baozun.
+ * You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Baozun.
+ *
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ */
+package com.yaotrue.base.thread;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author <a href="mailto:zhen.yao@baozun.cn">zhen.yao</a>
+ * 2016年5月26日 下午7:02:40
+ */
+public class ThreadOne extends Thread {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ThreadOne.class);
+	
+	private String name;
+	
+	/**
+	 * Creates a new instance of ThreadOne.
+	 */
+	public ThreadOne(String name) {
+		this.name = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Thread#run()
+	 */
+	@Override
+	public void run() {
+		for(int i=0;i<5;i++){
+			logger.info("{}运行{}",name,i);
+			try {
+				sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ yaotrue-common/src/test/java/com/yaotrue/base/thread/ThreadTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2012 Baozun All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Baozun.
+ * You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Baozun.
+ *
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ */
+package com.yaotrue.base.thread;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sourceforge.groboutils.junit.v1.MultiThreadedTestRunner;
+import net.sourceforge.groboutils.junit.v1.TestRunnable;
+
+/**
+ * @author <a href="mailto:zhen.yao@baozun.cn">zhen.yao</a> 2016年5月26日 下午7:05:48
+ */
+public class ThreadTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ThreadOne.class);
+
+	@Test
+	public void testThreadOne() {
+		ThreadOne threadOneA = new ThreadOne("A");
+		ThreadOne threadOneB = new ThreadOne("B");
+
+		threadOneA.start();
+		threadOneB.start();
+	}
+
+	public static void main(String[] args) {
+		ThreadOne threadOneA = new ThreadOne("A");
+		ThreadOne threadOneB = new ThreadOne("B");
+
+		threadOneA.start();
+		threadOneB.start();
+	}
+
+	@Test
+	public void MultiRequestsTest() {
+		// 构造一个Runner
+		TestRunnable runner = new TestRunnable() {
+			@Override
+			public void runTest() throws Throwable {
+				// 测试内容
+				for(int i=0;i<5;i++){
+					logger.info("{}运行{}",this.getClass().getName(),i);
+				}
+			}
+		};
+		int runnerCount = 100;
+		// Rnner数组，想当于并发多少个。
+		TestRunnable[] trs = new TestRunnable[runnerCount];
+		for (int i = 0; i < runnerCount; i++) {
+			trs[i] = runner;
+		}
+		// 用于执行多线程测试用例的Runner，将前面定义的单个Runner组成的数组传入
+		MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
+		try {
+			// 开发并发执行数组里定义的内容
+			mttr.runTestRunnables();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+}
